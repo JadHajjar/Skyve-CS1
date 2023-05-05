a6645e8d1674f6655279f4b997069f4f1b7669db
JadHajjar <jad@brains-lb.com>
2023-05-05 15:24:56 +0300

    Update PC_CompatibilityReport.Designer.cs

--- LoadOrderToolTwo/UserInterface/Panels/PC_CompatibilityReport.Designer.cs
+++ LoadOrderToolTwo/UserInterface/Panels/PC_CompatibilityReport.Designer.cs
@@ -28,26 +28,91 @@ partial class PC_CompatibilityReport
 	/// </summary>
 	private void InitializeComponent()
 	{
-			this.label1 = new System.Windows.Forms.Label();
+			SlickControls.DynamicIcon dynamicIcon3 = new SlickControls.DynamicIcon();
+			SlickControls.DynamicIcon dynamicIcon4 = new SlickControls.DynamicIcon();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.B_Manage = new SlickControls.SlickButton();
+			this.B_YourPackages = new SlickControls.SlickButton();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// label1
+			// base_Text
 			// 
-			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.label1.Location = new System.Drawing.Point(5, 30);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(773, 403);
-			this.label1.TabIndex = 13;
-			this.label1.Text = " Coming Soon..";
-			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.base_Text.Location = new System.Drawing.Point(-2, 3);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 30);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(783, 408);
+			this.tableLayoutPanel1.TabIndex = 13;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.tableLayoutPanel2.AutoSize = true;
+			this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel2.ColumnCount = 2;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel2.Controls.Add(this.B_Manage, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.B_YourPackages, 1, 0);
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(552, 0);
+			this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 1;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(231, 36);
+			this.tableLayoutPanel2.TabIndex = 0;
+			// 
+			// B_Manage
+			// 
+			this.B_Manage.ColorShade = null;
+			this.B_Manage.Cursor = System.Windows.Forms.Cursors.Hand;
+			dynamicIcon3.Name = "I_Cog";
+			this.B_Manage.ImageName = dynamicIcon3;
+			this.B_Manage.Location = new System.Drawing.Point(3, 3);
+			this.B_Manage.Name = "B_Manage";
+			this.B_Manage.Size = new System.Drawing.Size(100, 30);
+			this.B_Manage.SpaceTriggersClick = true;
+			this.B_Manage.TabIndex = 0;
+			this.B_Manage.Text = "Manage";
+			// 
+			// B_YourPackages
+			// 
+			this.B_YourPackages.ColorShade = null;
+			this.B_YourPackages.Cursor = System.Windows.Forms.Cursors.Hand;
+			dynamicIcon4.Name = "I_User";
+			this.B_YourPackages.ImageName = dynamicIcon4;
+			this.B_YourPackages.Location = new System.Drawing.Point(109, 3);
+			this.B_YourPackages.Name = "B_YourPackages";
+			this.B_YourPackages.Size = new System.Drawing.Size(119, 30);
+			this.B_YourPackages.SpaceTriggersClick = true;
+			this.B_YourPackages.TabIndex = 0;
+			this.B_YourPackages.Text = "YourPackages";
 			// 
 			// PC_CompatibilityReport
 			// 
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-			this.Controls.Add(this.label1);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.LabelBounds = new System.Drawing.Point(-2, 3);
 			this.Name = "PC_CompatibilityReport";
+			this.Padding = new System.Windows.Forms.Padding(0, 30, 0, 0);
 			this.Controls.SetChildIndex(this.base_Text, 0);
-			this.Controls.SetChildIndex(this.label1, 0);
+			this.Controls.SetChildIndex(this.tableLayoutPanel1, 0);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.tableLayoutPanel2.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -55,5 +120,8 @@ partial class PC_CompatibilityReport
 
 	#endregion
 
-	private System.Windows.Forms.Label label1;
+	private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+	private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+	private SlickControls.SlickButton B_Manage;
+	private SlickControls.SlickButton B_YourPackages;
 }
